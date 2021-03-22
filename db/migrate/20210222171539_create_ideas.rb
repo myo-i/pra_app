dbe1b8d40db0c9724417c09ddb39e1e6112e65ec
myo-i <Imiyo0521@gmail.com>
2021-03-23 02:03:28 +0900

    DB変更、新規作成ページにtitle追加

--- db/migrate/20210222171539_create_ideas.rb
+++ db/migrate/20210222171539_create_ideas.rb
@@ -1,8 +1,10 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
-      t.string       :name
-      t.string       :text
+      t.integer       :user_id
+      t.string        :title, null: false
+      t.text          :idea_1, null: false
+      t.text          :idea_2, null: false
       t.timestamps
     end
   end
